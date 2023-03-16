249c40b8d9e2ed4887ef46195e659a054f3dba25
Joel-Ch <joelchiappetti@gmail.com>
2023-03-16 11:30:16 +0000

    maze navigation again!

--- Week7/MazeNavigationUpdated/MazeNavigationUpdated.ino
+++ Week7/MazeNavigationUpdated/MazeNavigationUpdated.ino
@@ -4,11 +4,15 @@
 #include "ReturnAngleTurned.h"
 #include <string>
 #include <LiquidCrystal.h>
+#include <cctype>
+
+#define inputLength 30
 
 char alpha;
 const int rs = 19, en = 23, d4 = 18, d5 = 17, d6 = 16, d7 = 15;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int instructions[30], i = 0;
+int instructions[inputLength], i = 0;
+String distance[inputLength];
 String message;
 
 byte smiley[8] = {
@@ -107,7 +111,7 @@ void loop()
     lcd.print("   Welcome!");
     delay(3000);
     lcd.clear();
-    lcd.print("   Press 5 to");
+    lcd.print("   Press # to");
     lcd.setCursor(0, 1);
     lcd.print(" Continue!");
     lcd.setCursor(11, 1);
@@ -118,19 +122,19 @@ void loop()
     lcd.write(byte(4));
 
     alpha = customKeypad.waitForKey();
-  } while (alpha != '5');
+  } while (alpha != '#');
   // store all of the inputs into an array, confirm with the user and then execute the commands one by one.
 
   // Prompt for input and give them the choice to exit selection and begin movement
   lcd.clear();
   lcd.print("2|4|6|8 Movement");
   lcd.setCursor(0, 1);
-  lcd.print("[5 To Continue]"); // while loop is exited through pressing 5 and now we are given movement key
+  lcd.print("✓:# X:*"); // while loop is exited through pressing 5 and now we are given movement key
 
   i = 0;
   char enteredValue = customKeypad.waitForKey();
 
-  while (enteredValue != '5')
+  while (enteredValue != '#')
   { // until we choose to continue we will assign movement to the arrray elements
     lcd.clear();
     lcd.setCursor(2, 0);
@@ -140,11 +144,34 @@ void loop()
     Serial.print(i);
     Serial.print("\t");
     Serial.print("Value in Array = "); // Using for troubleshooting
-    Serial.println((char)instructions[i]);
+    Serial.println(instructions[i]);
     if (enteredValue == '2')
     {
       message = message + "F";
       instructions[i] = 2;
+      
+      char input='0';
+      do 
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Enter Distance:");
+        lcd.setCursor(0, 1);
+        lcd.print(distance[i]);
+        input = customKeypad.waitForKey();
+        if (input == '*')
+        {
+          if (distance[i].length() != 0)
+            distance[i].remove(distance[i].length()-1,1);
+        }
+        else
+
+          distance[i] += input;
+      } while (input != '#');
+      distance[i].remove(distance[i].length()-1,1);
+      message += distance[i];
+      lcd.clear();
+      Serial.print(distance[i]);
       i++;
     }
     else if (enteredValue == '4')
@@ -165,23 +192,39 @@ void loop()
       instructions[i] = 8;
       i++;
     }
+    else if (enteredValue == '*')
+    {
+      i--;
+      while (isdigit(message.charAt(message.length()-1)))
+      {
+        message.remove(message.length()-1,1);
+      }
+      
+      message.remove(message.length()-1,1);
+      instructions[i] = 0;
+      distance[i] = "";
+    }
+    lcd.setCursor(2, 0);
+    lcd.print("{Directions}");
+    lcd.setCursor(0,1);
     lcd.print(message);
 
     enteredValue = customKeypad.waitForKey();
   }
-  Serial.print("Value in position 1: ");
-  Serial.println(instructions[0]);
-  Serial.print("Value in position 2: ");
-  Serial.println(instructions[1]);
-  Serial.print("Value in position 3: ");
-  Serial.println(instructions[2]);
-  Serial.print("Value in position 4: ");
-  Serial.println(instructions[3]);
+
+  //inputs are in! time for execution
+  for (i = 0; i < inputLength; i++)
+  {
+    Serial.print("Position ");
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.println(instructions[i]);
+  }
 
   lcd.clear();
   lcd.print("     Happy?");
   lcd.setCursor(0, 1);
-  lcd.print(" 5 - ");
+  lcd.print(" # - ");
   lcd.setCursor(6, 1);
   lcd.write(byte(1));
   lcd.print(" |  * - ");
@@ -189,62 +232,73 @@ void loop()
   lcd.write(byte(2));
 
   char confirm = customKeypad.waitForKey();
-
-  if (confirm == '*')
+  while (confirm != '#' && confirm != '*')
   {
-    lcd.clear();
-    lcd.print("   Resetting...");
-    lcd.setCursor(0, 1);
-    lcd.print("  ¯|_(ツ)_/¯");
-    delay(3000);
-
-    return;
+    confirm = customKeypad.waitForKey();
   }
-  else if (confirm == '5')
+  if (confirm == '#')
   {
+    Serial.println("Confirmed!");
     lcd.clear();
     lcd.print(" Don't Touch!");
     lcd.setCursor(0, 1);
     lcd.print("(ง'̀-'́)ง");
-    for (i = 0; i <= 30; i++)
+    for (i = 0; i <= inputLength; i++)
     {
 
-      if (instructions[i] == '2')
+      if (instructions[i] == 2)
       {                           // Forwards
-        RunMotors(-130, 130, 85); // Assuming 90 degrees is centered for the vehicle.
-        delay(370);
+        RunMotors(-130, 130, 92); // Assuming 90 degrees is centered for the vehicle.
+        for(int j = 0; j < distance[i].toInt(); j++)
+        {
+          Serial.println(j);
+          delay(370);
+        }
         StopMotors();
       }
 
-      else if (instructions[i] == '4')
+      else if (instructions[i] == 4)
       { // Left
-        RunMotors(-120, 140, 60);
+        RunMotors(-120, 140, 62);
         ReturnAngleTurned(165);
       }
 
-      else if (instructions[i] == '6')
+      else if (instructions[i] == 6)
       { // Right
-        RunMotors(-140, 120, 120);
-        ReturnAngleTurned(165);
+        RunMotors(-140, 120, 122);
+        ReturnAngleTurned(178);
       }
 
-      else if (instructions[i] == '8')
+      else if (instructions[i] == 8)
       {
-        RunMotors(130, -130, 85); // Assuming 90 degrees is centered for the vehicle.
+        RunMotors(130, -130, 92); // Assuming 90 degrees is centered for the vehicle.
         delay(370);
         StopMotors();
       }
     }
+    lcd.clear();
+    lcd.print("  Task Complete!");
+    lcd.setCursor(8, 1);
+    lcd.write(byte(0));
+    delay(4000);
   }
 
   lcd.clear();
-  lcd.print("  Task Complete!");
-  lcd.setCursor(8, 1);
-  lcd.write(byte(0));
-  delay(4000);
+  lcd.print("   Resetting...");
+  lcd.setCursor(0, 1);
+  lcd.print("  ¯|_(ツ)_/¯");
+  delay(3000);
 
-  for (i = 0; i < 30; i++)
+  for (i = 0; i < inputLength; i++)
   {
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.print(instructions[i]);
+    Serial.print(" : ");
+    Serial.println(distance[i]);
     instructions[i] = 0;
+    distance[i] = "";
   }
+  message = "";
+  
 }
